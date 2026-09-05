22ac06f87b47516a935b1c34ff2b48229368e028
rachelname <rachel.net@gmail.com>
2026-09-05 17:29:34 +0630

    Initial YMain app

---
+++ src/main/java/y/app/YMainApplication.java
@@ -0,0 +1,13 @@
+package y.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/y/app/YMainApplicationTests.java
@@ -0,0 +1,13 @@
+package y.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
