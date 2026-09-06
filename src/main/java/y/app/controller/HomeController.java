11e9021e90433aba12a4c8a2fde3308f2e966bc1
rachelname <rachel.net@gmail.com>
2026-09-06 16:01:44 +0630

    update home

--- src/main/java/y/app/controller/HomeController.java
+++ src/main/java/y/app/controller/HomeController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+
 public class HomeController {
  @GetMapping("/")
  public String home() {
 	 return "home";
  }
- 
+ @GetMapping("/add")
+ public String create() {
+	 return "add";
+ }
  
 }
