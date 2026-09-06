ddf1eb70e69edf571d38b8028755038ed928c2a1
rachelname <rachel.net@gmail.com>
2026-09-06 16:23:42 +0630

    update post

--- src/main/java/y/app/controller/PostController.java
+++ src/main/java/y/app/controller/PostController.java
@@ -1,5 +1,11 @@
 package y.app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PostController {
 
+	//test
+	
 }
